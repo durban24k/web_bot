bd2f0e2d8a17827bbd1c3f9501c1822bbc4cd5b2
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-10-16 17:40:47 +0300

    login v2.0

--- bot.py
+++ bot.py
@@ -15,9 +15,13 @@ def timeme(method):
 
 def login(x):
      driver.get(x['url'])
-     driver.find_element_by_xpath('//*[@id="userName"]').send_keys(x["email"])
-     driver.find_element_by_xpath('//*[@id="password"]').send_keys(x["password"])
-     driver.find_element_by_xpath('//*[@id="btnLogin"]').click()
+     driver.find_element_by_xpath('//*[@id="id_esauth_myaccount_login_link"]').click()
+     driver.find_element_by_xpath('//*[@id="id_esauth_login_field"]').send_keys(x["email"])
+     driver.find_element_by_xpath('//*[@id="id_esauth_pwd_field"]').send_keys(x["password"])
+     driver.find_element_by_xpath('//*[@id="stay_signed_in"]').click()
+     driver.find_element_by_xpath('//*[@id="id_esauth_login_button"]').click()
+     time.sleep(4)
+     driver.find_element_by_xpath('/html/body/div[2]/div/div[5]/div/div[2]/table/tbody/tr[12]/td[2]/a').click()
 
 if __name__ == '__main__':
      driver=webdriver.Chrome('./chromedriver')
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
+import time
+
 def hello():
      print(f'Hello World')
      def sum():
           print(2)
+     while True:
+          sum()
+          time.sleep(3)
+
 
 if __name__ == '__main__':
-     while True:
-          hello()
+     hello()
